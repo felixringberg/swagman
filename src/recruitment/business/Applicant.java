3d757c792a1c0aa63be92da00c6e518dc2c2cf0e
felixringberg <felixri@kth.se>
2015-02-09 22:37:18 +0100

    la till java persistence unit

--- src/recruitment/business/Applicant.java
+++ src/recruitment/business/Applicant.java
@@ -5,10 +5,54 @@
  */
 package recruitment.business;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Felix Ringberg <felixri at kth.se>
  */
-public class Applicant {
+@Entity
+public class Applicant implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Applicant)) {
+            return false;
+        }
+        Applicant other = (Applicant) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "recruitment.business.Applicant[ id=" + id + " ]";
+    }
     
 }
