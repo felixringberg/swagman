2ba8f509e75a91e1d649d65fdef29e2e3a7bfd21
felixringberg <felixri@kth.se>
2015-02-19 21:15:11 +0100

    la till validering på server

--- src/recruitment/business/Applicant.java
+++ src/recruitment/business/Applicant.java
@@ -26,18 +26,19 @@ public class Applicant implements Serializable, ApplicantDTO {
     }
     public Applicant(String firstname, String lastname, String dateofbirth, String email, 
             String username, String password) throws ValidationException {
-        // Validering för lösenord
-        if(password.length() < 6)
-            throw new ValidationException("Password too short");
-        for(int i = 0; i < password.length(); i++)
-            if(!(Character.isDigit(password.charAt(i)) || Character.isLetter(password.charAt(i))))
-               throw new ValidationException("Passwords may only contain letters A-Z, a-z or numbers 0-9");
-        
+        // Username validation
         if(username.length() < 6)
             throw new ValidationException("Password too short");
         for(int i = 0; i < username.length(); i++)
             if(!(Character.isDigit(username.charAt(i)) || Character.isLetter(username.charAt(i))))
                throw new ValidationException("Usernames may only contain letters A-Z, a-z or numbers 0-9");
+        
+        // Password validation
+        if(password.length() < 6)
+            throw new ValidationException("Password too short");
+        for(int i = 0; i < password.length(); i++)
+            if(!(Character.isDigit(password.charAt(i)) || Character.isLetter(password.charAt(i))))
+               throw new ValidationException("Passwords may only contain letters A-Z, a-z or numbers 0-9");
         
         this.firstname = firstname;
         this.lastname = lastname;
@@ -52,26 +53,14 @@ public class Applicant implements Serializable, ApplicantDTO {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
     @Override
     public String getLastname() {
         return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
     }
     
     @Override
     public String getDateofbirth() {
         return dateofbirth;
-    }
-    
-    public void setDateofbirth(String dateofbirth){
-        this.dateofbirth = dateofbirth;
     }
 
     @Override
@@ -79,26 +68,14 @@ public class Applicant implements Serializable, ApplicantDTO {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     @Override
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     @Override
@@ -125,5 +102,4 @@ public class Applicant implements Serializable, ApplicantDTO {
     public String toString() {
         return "recruitment.business.Applicant[ id=" + username + " ]";
     }
-    
 }
--- src/recruitment/integration/DatabaseFacade.java
+++ src/recruitment/integration/DatabaseFacade.java
@@ -11,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import recruitment.business.Applicant;
 import recruitment.business.ApplicantDTO;
+import recruitment.business.ValidationException;
 
 /**
  * Klassen ska ha nån koppling till databasen. Den ska hämta och lägga upp data.
@@ -22,7 +23,7 @@ public class DatabaseFacade {
     private EntityManager em;
     
     public ApplicantDTO registerApplicant(String firstname, String lastname, 
-            String dateofbirth, String email, String username, String password){
+            String dateofbirth, String email, String username, String password) throws ValidationException {
         if(em.find(Applicant.class, username) != null) {
             throw new EntityExistsException("Account already exists with username " + username);
         }
